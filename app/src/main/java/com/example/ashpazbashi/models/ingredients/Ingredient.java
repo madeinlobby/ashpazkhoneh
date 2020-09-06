9a3074902bcfed7dc420adaddda38a8b2ad6e242
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-09-06 12:34:16 +0430

    complete ingredient_row & make fields at Ingredient class & define onClick methods for buttons

--- app/src/main/java/com/example/ashpazbashi/models/ingredients/Ingredient.java
+++ app/src/main/java/com/example/ashpazbashi/models/ingredients/Ingredient.java
@@ -1,4 +1,39 @@
 package com.example.ashpazbashi.models.ingredients;
 
 public class Ingredient {
+    private String name;
+    private String amount;
+    private String unit;
+
+
+    public Ingredient(String name, String amount, String unit) {
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
--- app/src/main/java/com/example/ashpazbashi/views/IngredientsActivity.java
+++ app/src/main/java/com/example/ashpazbashi/views/IngredientsActivity.java
@@ -3,6 +3,7 @@ package com.example.ashpazbashi.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ashpazbashi.R;
 
@@ -12,5 +13,11 @@ public class IngredientsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+    }
+
+    public void addIngredientClick(View view) {
+    }
+
+    public void removeIngredientClick(View view) {
     }
 }
