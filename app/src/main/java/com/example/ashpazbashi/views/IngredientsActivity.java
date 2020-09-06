aa4dacd3a0f3d94453a77a2738946317557ff535
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-09-06 18:45:43 +0430

    complete ingredient recyclerView & its clickListeners ( not debugged)

--- app/src/main/java/com/example/ashpazbashi/views/IngredientsActivity.java
+++ app/src/main/java/com/example/ashpazbashi/views/IngredientsActivity.java
@@ -1,23 +1,42 @@
 package com.example.ashpazbashi.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.ashpazbashi.R;
+import com.example.ashpazbashi.models.ingredients.Ingredient;
+import com.example.ashpazbashi.views.recyclerViewAdaptors.IngredientAdaptor;
 
-public class IngredientsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class IngredientsActivity extends AppCompatActivity implements IngredientAdaptor.OnIngredientListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+
+        RecyclerView recyclerView = findViewById(R.id.ingredientsRecyclerView);
+        IngredientAdaptor adapter = new IngredientAdaptor(new ArrayList<Ingredient>(), this, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void addIngredientClick(View view) {
+        //show dialog and get data
     }
 
-    public void removeIngredientClick(View view) {
+    @Override
+    public void onIngredientItemCLick(int position) {
+        // when user clicks on an item
+    }
+
+    @Override
+    public void onDeleteIngredientClick(int position) {
+        // when user clicks on delete icon on item
     }
 }
--- app/src/main/java/com/example/ashpazbashi/views/recyclerViewAdaptors/IngredientAdaptor.java
+++ app/src/main/java/com/example/ashpazbashi/views/recyclerViewAdaptors/IngredientAdaptor.java
@@ -1,4 +1,102 @@
 package com.example.ashpazbashi.views.recyclerViewAdaptors;
 
-public class IngredientAdaptor {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ashpazbashi.R;
+import com.example.ashpazbashi.models.ingredients.Ingredient;
+
+import java.util.ArrayList;
+
+public class IngredientAdaptor extends RecyclerView.Adapter<IngredientAdaptor.ViewHolder> {
+    private ArrayList<Ingredient> data;
+    private LayoutInflater inflater;
+    private OnIngredientListener listener;
+
+    public IngredientAdaptor(ArrayList<Ingredient> data, Context context, OnIngredientListener listener) {
+        this.data = data;
+        this.inflater = LayoutInflater.from(context);
+        this.listener = listener;
+    }
+
+    public ArrayList<Ingredient> getData() {
+        return data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(inflater.inflate(R.layout.ingredient_row, parent, false), listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Ingredient ingredient = data.get(position);
+        holder.ingredientName.setText(ingredient.getName());
+        String string = ingredient.getAmount() + '/' + ingredient.getUnit();
+        holder.ingredientAmount.setText(string);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder  {
+
+
+        TextView ingredientName;
+        TextView ingredientAmount;
+        ImageView deleteIngredientImage;
+        OnIngredientListener onIngredientListener; // hamoon listenrer balayie :)
+
+        public ViewHolder(View itemView, final OnIngredientListener onIngredientListener) {
+            super(itemView);
+            ingredientName = itemView.findViewById(R.id.ingredientName);
+            ingredientAmount = itemView.findViewById(R.id.ingredientAmount);
+            deleteIngredientImage = itemView.findViewById(R.id.removeIngredientImage);
+            this.onIngredientListener = onIngredientListener;
+//            itemView.setOnClickListener(this);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    // check if position is valid
+                    onIngredientListener.onIngredientItemCLick(getAdapterPosition());
+                }
+            });
+
+
+            deleteIngredientImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // check if position is valid
+                    onIngredientListener.onDeleteIngredientClick(getAdapterPosition());
+                }
+            });
+
+        }
+
+//        @Override
+//        public void onClick(View view) {
+//            // check if position is valid
+//            onIngredientListener.onIngredientItemCLick(getAdapterPosition());
+//        }
+    }
+
+
+    public interface OnIngredientListener {
+        void onIngredientItemCLick(int position);
+        void onDeleteIngredientClick(int position);
+
+    }
+
+
 }
